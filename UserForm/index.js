228fc34050d0c7ee1ba72c5370178ee1214b5366
sakshiigothwal <119726876+sakshiigothwal@users.noreply.github.com>
2025-06-02 21:23:04 +0530

    Updated .js

--- UserForm/index.js
+++ UserForm/index.js
@@ -1,9 +1,10 @@
 const form = document.getElementById("registrationForm");
 const search = document.getElementById("search");
 const list = document.getElementById("list");
+const errMsg = document.getElementById("error")
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const passwordRegex = /^.{8,}$/; 
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordPattern = /^.{8,}$/; 
 
 function getUsers() {
   return JSON.parse(localStorage.getItem("users")) || [];
@@ -17,15 +18,20 @@ function displayUsers(users = getUsers()) {
   list.innerHTML = "";
 
   if (users.length === 0) {
-    list.innerHTML = "No users.";
+    list.innerHTML = `<tr><td colspan="2">No users.</td></tr>`;
     return;
   }
 
   users.forEach(user => {
-    const div = document.createElement("div");
-    div.classList.add("user-card");
-    div.innerHTML = `${user.name}<br>${user.email}`;
-    list.appendChild(div);
+    const row = document.createElement("tr");
+    const nameCol= document.createElement("td");
+    nameCol.textContent = user.name;
+    const emailCol = document.createElement("td");
+    emailCol.textContent = user.email;
+
+    row.appendChild(nameCol);
+    row.appendChild(emailCol);
+    list.appendChild(row);
   });
 }
 
@@ -36,13 +42,22 @@ form.addEventListener("submit", e => {
   const email = form.email.value.trim();
   const password = form.password.value.trim();
 
-  if (!emailRegex.test(email) || !passwordRegex.test(password)) {
+  errMsg.innerText="";
+
+  if (!emailPattern.test(email)) {
+    errMsg.innerText = "Please enter a valid email address.";
+    return;
+  }
+
+  if (!passwordPattern.test(password)) {
+    errMsg.innerText = "Password must be at least 8 characters long.";
     return;
   }
 
   const users = getUsers();
 
   if (users.some(u => u.email === email)) {
+    errMsg.innerText = "An account with this email already exists.";
     return;
   }
 
